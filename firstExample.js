baa4c3287bf0fea13de13a747aed6e2fb386998a
kevin101294 <kevin101294@gmail.com>
2024-05-16 17:08:31 +0000

    Mi primer archivo usando IDX + Gemini

---
+++ firstExample.js
@@ -0,0 +1,9 @@
+console.log("Bienvenidos a la clase de Programación Orientada a Objetos con 🐔")
+
+/* Tres comandos para mandar a nuestro repositorio
+
+1 - git add .
+2 - git commit -m "Mensaje"
+3 - git push origin master"
+
+*/
